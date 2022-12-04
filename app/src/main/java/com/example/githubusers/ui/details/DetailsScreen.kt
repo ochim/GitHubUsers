8f4708d2174ccf21118dbd8b6e7afced371cdac8
ochi <mune.ochi@gmail.com>
2022-12-04 18:40:12 +0900

    create HomeScreen and DetailsScreen

---
+++ app/src/main/java/com/example/githubusers/ui/details/DetailsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.githubusers.ui.details
+
+import androidx.compose.runtime.Composable
+import com.example.githubusers.Greeting
+
+@Composable
+fun DetailsScreen() {
+    Greeting("Details")
+}
---
+++ app/src/main/java/com/example/githubusers/ui/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.githubusers.ui.home
+
+import androidx.compose.runtime.Composable
+import com.example.githubusers.Greeting
+
+@Composable
+fun HomeScreen() {
+    Greeting("Home")
+}
