82bea6b8955b8193774d501e4b9e9efcf94855ea
ochi <mune.ochi@gmail.com>
2022-12-06 17:44:48 +0900

    show user info on details screen

--- app/src/main/java/com/example/githubusers/domain/User.kt
+++ app/src/main/java/com/example/githubusers/domain/User.kt
@@ -4,4 +4,12 @@ data class User(
     val login: String,
     val id: Int,
     val avatar_url: String?,
+    val html_url: String,
+    val name: String?,
+    val location: String?,
+    val email: String?,
+    val bio: String?,
+    val twitter_username: String?,
+    val followers: Int?,
+    val following: Int?,
 )
--- app/src/main/java/com/example/githubusers/ui/details/DetailsScreen.kt
+++ app/src/main/java/com/example/githubusers/ui/details/DetailsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.githubusers.ui.details
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -9,9 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Scale
 import com.example.githubusers.R
 import com.example.githubusers.domain.User
 import com.example.githubusers.ui.FetchNetworkModelState
@@ -81,11 +87,36 @@ fun DetailsContent(
             val user = (users.value as? FetchNetworkModelState.RefreshedOK<User>)?.data
             if (user != null) {
                 Column(
-                    modifier = Modifier.padding(top = paddingValues.calculateTopPadding()),
+                    modifier = Modifier.padding(all = 8.dp),
                 ) {
-                    Text(text = user.login)
-                    Text(text = "${user.id}")
-                    Text(text = user.avatar_url ?: "")
+                    user.avatar_url?.let {
+                        Image(
+                            modifier = Modifier
+                                .padding(bottom = 8.dp)
+                                .width(100.dp)
+                                .height(100.dp),
+                            painter = rememberAsyncImagePainter(
+                                ImageRequest.Builder(LocalContext.current).data(data = it)
+                                    .apply(block = fun ImageRequest.Builder.() {
+                                        crossfade(true)
+                                        scale(Scale.FILL)
+                                    }).build()
+                            ),
+                            contentDescription = null,
+                            contentScale = ContentScale.Fit
+                        )
+                    }
+                    val textStyle = MaterialTheme.typography.subtitle1
+                    Text(style = textStyle, text = user.login)
+                    Text(style = textStyle, text = user.name ?: "")
+                    Text(style = textStyle, text = user.location ?: "")
+                    Text(style = textStyle, text = "email: ${user.email ?: ""}")
+                    Text(style = textStyle, text = user.bio ?: "")
+                    user.twitter_username?.let {
+                        Text(style = textStyle, text = "twitter: @${user.twitter_username}")
+                    }
+                    Text(style = textStyle, text = "followers: ${user.followers}")
+                    Text(style = textStyle, text = "following: ${user.following}")
                 }
             } else {
                 Text(username)
